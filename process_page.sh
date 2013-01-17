91f5aff8262673d0e77abd5a195f519704857f13
Barry John Williams <barry@bjw.me.uk>
2013-01-17 18:52:11 +0000

    Reorganised template and generation files

--- process_page.sh
+++ process_page.sh
@@ -3,7 +3,7 @@
 # Author: Barry John Williams
 # Creative Commons Attribute-Share Alike 2.5 UK:Scotland Licence
 
-TARGETFOLDER=.
+TARGETFOLDER=./template
 FILENAME=$1
 NAME=${FILENAME%\.*}
 python process_page.py ${NAME}.xml xsl/html5.xsl > ${TARGETFOLDER}/${NAME}.html
